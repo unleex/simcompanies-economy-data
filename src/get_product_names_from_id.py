d8319a02e489b3337ee632807ea86fd03ae76ff4
Maxim Degi <unliex@yandex.ru>
2024-09-21 23:27:11 +0300

    implemented getting market tickers, created json with id to product name

---
+++ src/get_product_names_from_id.py
@@ -0,0 +1,14 @@
+import datetime
+import json
+
+from simcompanies_api import get_market_ticker
+
+product_id_to_name = {}
+for product in get_market_ticker(date_time=datetime.datetime.today(), realm=0, get_last_marker=True):
+    image_name =  product["image"]
+    id_ = product["kind"]
+    name = image_name[image_name.rfind('/') + 1 : image_name.find('.')]
+    product_id_to_name[id_] = name
+
+with open("src/product_id_to_name.json", "w") as fp:
+    json.dump(product_id_to_name, fp, indent="\t")
---
+++ src/simcompanies_api.py
@@ -0,0 +1,43 @@
+import datetime
+import requests
+from typing import *
+
+
+MARKET_TICKER_UPDATE_PERIOD_MINUTES = 60 * 4
+
+
+class SimcompaniesAPIError(Exception):
+    ...
+
+
+def get_market_ticker(date_time: datetime.datetime, 
+                      realm: Literal[0, 1],
+                      get_last_marker: bool = False
+    ) -> list[dict[str, str]]:
+    """
+    Access market ticker in specific datetime and realm
+        
+    Parameters
+    ----------------
+    datetime: datetime.datetime
+        Datetime for market ticker
+    realm: Literal[0, 1]
+        Realm. 0 for Magnates, 1 for Enterprineurs.
+    get_last_marker: bool (default is False)
+        Whether to correct given date_time to last available time marker, otherwise raise error if marker is not available.
+    """
+    def _get_time_marker(date_time: datetime.datetime):
+        """Get current time marker in format '%Y-%m-%dT%H:%M:%S.%fZ', truncating to milliseconds"""
+        return date_time.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z"
+    
+
+    if get_last_marker:
+        date_time -= datetime.timedelta(minutes=MARKET_TICKER_UPDATE_PERIOD_MINUTES)
+    time_marker = _get_time_marker(date_time)
+    response = requests.get(f"https://www.simcompanies.com/api/v2/market-ticker/{realm}/{time_marker}/")
+    if response.status_code != 200:
+        raise SimcompaniesAPIError()
+    if not response.json():
+        raise SimcompaniesAPIError(f"Found no data for realm {realm} at {time_marker}")
+    
+    return response.json()
