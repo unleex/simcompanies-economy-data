3b4d35309dd48396e1eaa3ce83f3236adc69b57c
Maxim Degi <unliex@yandex.ru>
2024-11-08 22:54:04 +0300

    Rewrote logic of rendering buttons. Added geometry to graph window.

--- src/app.py
+++ src/app.py
@@ -25,20 +25,20 @@ class MarketGraphWindow(QMainWindow):
         """
         Find how nested the graph is.
         """
-        def max_depth(graph, depth=0):
+        def wrapper(graph, depth=0):
             for item in graph:
                 if isinstance(graph[item], dict):
-                    depth = max_depth(graph[item], depth=depth+1)
+                    depth = wrapper(graph[item], depth=depth+1)
             return depth + 1
         
-        return max_depth(graph)
+        return wrapper(graph)
 
 
     def _get_item_positions(
             self,
             graph: dict[str, list[str] | dict], 
             size: QSize
-        ) -> dict[str, tuple[float, float]]:
+        ) -> dict[str, tuple[int, int]]:
         """
         Calculate positions of the graph for arranging it's items as buttons
         in fixed-size GUI window.
@@ -49,67 +49,64 @@ class MarketGraphWindow(QMainWindow):
             Graph for arranging
         size: QSize
             Size of the window to arrange graph in
+        
+        Returns
+        --------
+        item_positions: dict[str, tuple[int, int]]
+            x and y positions of each graph item
         """
-        x_step = round(size.width() / self._get_graph_max_depth(graph))
+        
 
-        def get_item_positions(
+        def wrapper(
             graph: dict[str, list[str] | dict], 
             size: QSize,
-            layer_size: int=0,
             depth: int=0,
-            x_step: int=0
-        ) -> dict[str, tuple[float, float]]:
+            x_step: int=0,
+            y_align:int=0
+        ) -> dict[str, tuple[int, int]]:
             """
             Parameters
             ---------
-            layer_size: int (first call is 0)
-                Amount of products on the same graph depth,
-                including from other branches too.
+            graph: dict[str, list[str] | dict]
+                Graph for arranging
+            size: QSize
+                Size of the window to arrange graph in
             depth: int (first call is 0)
-                Depth of current layer. Used to calculate x position.
+                Depth of current layer. Used with x_step to calculate x position.
             x_step: int (first call is 0)
-                Gap between graph layers. 
+                Gap between graph layers.
+            y_align: int (first call is 0)
+                Shift on y axis applied on item to align them on its parent 
+            
+            Returns
+            --------
+            item_positions: dict[str, tuple[int, int]]
+                x and y positions of each graph item
             """
-            if not layer_size:
-                layer_size = len(graph)
-            item_positions: dict[str, tuple[float, float]] = {}
-            # size of gaps between items on y axis. there is more gaps than items by 1
-            # take other branches into account not to collide with them
-            y_step = round(size.height() / (layer_size + 1))
-            # shift by half for symmetry if length is even
-            if len(graph) % 2 == 0:
-                symmetrical_y_shift = y_step // 2
-            else:
-                symmetrical_y_shift = 0
+            item_positions: dict[str, tuple[int, int]] = {}
+            y_step = round(size.height() / len(graph))
+            y_shift = round(size.height() / 2 / len(graph))
+            x = x_step * depth
             for i, item in enumerate(graph):
-                x = x_step * depth
-                # leave gap from border, so +1
-                y = y_step * (i + 1) + symmetrical_y_shift
+                y = y_step * i + y_shift + y_align
                 item_positions[item] = (x, y)
-                if isinstance(graph[item], list):
-                    # fit in gap size of parent item
-                    y_step_next = round(y_step / len(graph[item]))
-                    # add height of parent to align the child
-                    y_shift = y // len(graph[item])
-                    for j, next_item in enumerate(graph[item]): 
-                        x_next = x_step * (depth + 1)
-                        # don't forget parent's shift!
-                        y_next = y_step_next * (j + 1) + y_shift + symmetrical_y_shift
-                        item_positions[next_item] = (x_next, y_next)
-                else:
-                    next_height = round(size.height() / (len(graph) + 1))
+                if isinstance(graph, dict):
+                    next_height = round(size.height() / len(graph))
                     next_size = QSize(size.width(), next_height)
                     item_positions.update(
-                        get_item_positions(
+                        wrapper(
                             graph=graph[item], # type: ignore
                             size=next_size,
                             depth=depth+1,
                             x_step=x_step,
-                            layer_size=len(graph.values())
+                            y_align=y_step*i
                         )
-                    ) # type: ignore
+                    ) 
             return item_positions
-        item_positions = get_item_positions(
+        
+
+        x_step = round(size.width() / self._get_graph_max_depth(graph))
+        item_positions = wrapper(
             graph=graph,
             size=size,
             x_step=x_step
@@ -121,7 +118,9 @@ class MarketGraphWindow(QMainWindow):
         """
         Render items of the graph as buttons on window
         """
+        self.setGeometry(0, 0, self.width(), self.height())
         positions = self._get_item_positions(self.graph, self.size())
+        
         for name, position in positions.items():
             button = QPushButton(name, self)
             button.move(*position)
@@ -135,7 +134,7 @@ graph: dict[str, list[str] | dict] = {
                 "stiletto heel", 
                 "handbags",
                 "gloves", 
-                "luxury car interior"
+                # "luxury car interior"
             ],
         "steak":
         [
@@ -146,7 +145,6 @@ graph: dict[str, list[str] | dict] = {
 }
 app = QApplication(sys.argv)
 window = MarketGraphWindow(graph=graph)
-print(window._get_item_positions(window.graph, window.size()))
 window.render_graph()
 window.show()
 app.exec()
